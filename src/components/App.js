84b7ac1e76eaae7a9150a230e8884f6a1aaca239
Pierre <pierre@example.local>
2025-01-29 09:46:51 +0100

    ajout d'un pannier

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import Banner from './banner';
+import Banner from './Banner';
+import Cart from './Cart';
 
 function App() {
   return (
+    <div>
       <Banner/>
-  );
+      <Cart />
+  </div>);
 }
 
 export default App;
---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Cart.css';
+
+const Cart = () => {
+  return (
+    <div className="lmj-cart">
+     Mon panier
+    </div>
+  );
+};
+
+export default Cart;
