1f39aaa184c8374bdee278c8c7bf9e3ab15e1ae1
Pierre <pierre@example.local>
2025-01-29 09:43:00 +0100

    ajout css et logo

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,6 +1,12 @@
+import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
 function Banner(){
-    return <div className="lmj-banner"> <h1>La maison jungle</h1></div>
+    const title = 'PlantShop'
+    return <div className="lmj-banner"> 
+    
+    <img src = {logo} alt="PlantShop Logo" className='lmj-logo'></img>
+    <h1>{title}</h1>
+    </div>
 }
 export default Banner
