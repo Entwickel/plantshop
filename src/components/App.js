ffe19447574ee4d8e5505643fa2e99ed8643808e
Pierre <pierre@example.local>
2025-01-31 10:59:34 +0100

    ajout footer et ajout dependances

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import Banner from './Banner';
 import Cart from './Cart';
 import ShoppingList from './ShoppingList';
+import React from 'react';
+import {useState} from 'react'
+import logo from '../assets/logo.png'
+import Footer from './Footer';
 
 function App() {
+  const [cart, updateCart] = useState([])
+  
   return (
-    <div>
-      <Banner/>
-      <Cart />
-      <ShoppingList></ShoppingList>
-  </div>);
+      <div>
+          <Banner>
+              <img src={logo} alt='La maison jungle' className='lmj-logo' />
+              <h1 className='lmj-title'>La maison jungle</h1>
+          </Banner>
+          <div className='lmj-layout-inner'>
+              <Cart cart={cart} updateCart={updateCart} />
+              <ShoppingList cart={cart} updateCart={updateCart} />
+          </div>
+          <Footer />
+      </div>
+  )
 }
 
 export default App;
