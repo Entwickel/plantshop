49bb08e34d472acd3871a9ac0254c256f1e61067
Pierre <pierre@example.local>
2025-01-31 10:32:45 +0100

    ajout d'un panier poc

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import '../styles/Cart.css';
+import {useState} from 'react'
 
-const Cart = () => {
+
+function Cart(){
+  const monsteraPrice = 8
+  const [cart, updateCart] = useState(0)
+
   return (
-    <div className="lmj-cart">
-     Mon panier
-    </div>
-  );
-};
+    <div className='lmj-cart'>
+      <h2>Panier</h2>
+      <div>
+          Monstera : {monsteraPrice}€
+          <button onClick={() => updateCart(cart + 1)}>
+              Ajouter
+          </button>
+      </div>
+      <h3>Total : {monsteraPrice * cart}€</h3>
+  </div> 
+      )
+}
 
 export default Cart;
